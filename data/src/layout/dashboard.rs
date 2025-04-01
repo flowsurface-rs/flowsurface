86abcbdbdbe50c196960ea42ac1dbd1f930f69bd
akenshaw <63060680+akenshaw@users.noreply.github.com>
2025-04-01 20:13:57 +0300

    handle pane migrations more gracefully while deserializing from saved state

--- data/src/layout/dashboard.rs
+++ data/src/layout/dashboard.rs
@@ -1,10 +1,13 @@
 use serde::{Deserialize, Serialize};
 
 use super::{WindowSpec, pane::Pane};
+use crate::layout::pane::ok_or_default;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Dashboard {
+    #[serde(deserialize_with = "ok_or_default")]
     pub pane: Pane,
+    #[serde(deserialize_with = "ok_or_default")]
     pub popout: Vec<(Pane, WindowSpec)>,
     pub trade_fetch_enabled: bool,
 }
@@ -12,7 +15,7 @@ pub struct Dashboard {
 impl Default for Dashboard {
     fn default() -> Self {
         Self {
-            pane: Pane::Starter,
+            pane: Pane::default(),
             popout: vec![],
             trade_fetch_enabled: false,
         }
