d847f0f541409fc468b9877c91d4995ca95f0897
akenshaw <63060680+akenshaw@users.noreply.github.com>
2025-04-18 19:33:03 +0300

    fix indicator reordering for `FootprintChart`

--- src/chart/footprint.rs
+++ src/chart/footprint.rs
@@ -697,35 +697,37 @@ impl FootprintChart {
             None => return indicators,
         };
 
-        for indicator in I::get_enabled(enabled, market) {
-            if let Some(candlestick_indicator) =
-                indicator.as_any().downcast_ref::<FootprintIndicator>()
-            {
-                match candlestick_indicator {
-                    FootprintIndicator::Volume => {
-                        if let Some(IndicatorData::Volume(cache, data)) =
-                            self.indicators.get(&FootprintIndicator::Volume)
-                        {
-                            indicators.push(indicator::volume::create_indicator_elem(
-                                chart_state,
-                                cache,
-                                data,
-                                earliest,
-                                latest,
-                            ));
+        for indicator in enabled {
+            if I::get_available(market).contains(indicator) {
+                if let Some(candlestick_indicator) =
+                    indicator.as_any().downcast_ref::<FootprintIndicator>()
+                {
+                    match candlestick_indicator {
+                        FootprintIndicator::Volume => {
+                            if let Some(IndicatorData::Volume(cache, data)) =
+                                self.indicators.get(&FootprintIndicator::Volume)
+                            {
+                                indicators.push(indicator::volume::create_indicator_elem(
+                                    chart_state,
+                                    cache,
+                                    data,
+                                    earliest,
+                                    latest,
+                                ));
+                            }
                         }
-                    }
-                    FootprintIndicator::OpenInterest => {
-                        if let Some(IndicatorData::OpenInterest(cache, data)) =
-                            self.indicators.get(&FootprintIndicator::OpenInterest)
-                        {
-                            indicators.push(indicator::open_interest::create_indicator_elem(
-                                chart_state,
-                                cache,
-                                data,
-                                earliest,
-                                latest,
-                            ));
+                        FootprintIndicator::OpenInterest => {
+                            if let Some(IndicatorData::OpenInterest(cache, data)) =
+                                self.indicators.get(&FootprintIndicator::OpenInterest)
+                            {
+                                indicators.push(indicator::open_interest::create_indicator_elem(
+                                    chart_state,
+                                    cache,
+                                    data,
+                                    earliest,
+                                    latest,
+                                ));
+                            }
                         }
                     }
                 }
